732a30482b9cc8635713b93798e65853a9c7d7e4
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2025-06-12 10:54:20 +0700

    Delete translateRule directory

--- translateRule/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request
-from select_and_show.select_and_show import show_rule_bp
-app = Flask(__name__)
-app.register_blueprint(show_rule_bp,template_folder="select_and_show/templates", static_folder = "select_and_show/static")
-
-if __name__ == '__main__':
-    app.run(debug=True)
